9f84e0adca16a6d119e2ac79cf1675fa970e652f
minh la <satthutit@gmail.com>
2018-10-09 14:17:40 +0900

    Done kumiaiinnitiran page

---
+++ page-kumiaiinitiran.php
@@ -0,0 +1,229 @@
+<?php get_header(); ?>
+
+<main class="clear">
+    <section id="header-text-block">
+        <div class="container">
+            <div class="wrap-header-txt">
+                <div class="title">組合員一覧</div>
+                <div class="desc">Member List</div>
+            </div>
+        </div>
+    </section>
+    <section id="gmap">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3272.8601604587475!2d136.92100361512237!3d34.884857680387576!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x600485b7edc37809%3A0x14b88138131ae3ad!2z44CSNDc1LTA4NDYgQWljaGkta2VuLCBIYW5kYS1zaGksIFNha2FlbWFjaGksIDMgQ2hvbWXiiJIxMjMg55-l5aSa5Y2w5Yi377yI5qCq77yJ!5e0!3m2!1svi!2sjp!4v1537584242588" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+    </section>
+    <section id="kumiaiinitiran-content">
+        <div class="container">
+            <div class="row">
+                <div class="top-content">
+                    <div class="top-title">
+                        <i class="icon-kumiai_search_icon"></i>地域でみる
+                    </div>
+                    <div class="top-button">
+                        <a href="#">東京エリア</a>
+                        <a href="#">神奈川エリア</a>
+                        <a href="#">埼玉エリア</a>
+                        <a href="#">千葉エリア</a>
+                        <a href="#">茨城エリア</a>
+                        <a href="#">栃木エリア</a>
+                        <a href="#">群馬エリア</a>
+                        <a href="#">すべて</a>
+                    </div>
+                </div>
+                <div class="main-content">
+                    <div class="wrap-item">
+                        <div class="title">東京エリア</div>
+                        <div class="location-box">
+                            <div class="box-item">
+                                <div class="name">社　名</div>
+                                <div class="desc">株式会社 交運社</div>
+                            </div>
+                            <div class="box-item">
+                                <div class="name">住　所</div>
+                                <div class="desc">〒197-0011 東京都福生市福生654</div>
+                            </div>
+                            <div class="box-item">
+                                <div class="name">TEL</div>
+                                <div class="desc">042-551-0541</div>
+                            </div>
+                            <div class="box-item">
+                                <div class="name">FAX</div>
+                                <div class="desc">042-553-6501</div>
+                            </div>
+                            <div class="box-item">
+                                <div class="name">URL</div>
+                                <div class="desc">http://www.kohunsha.co.jp/</div>
+                            </div>
+                        </div>
+                        <div class="last-line">
+                            <img src="./images/line.svg" alt="">
+                        </div>
+                        <div class="location-box">
+                            <div class="box-item">
+                                <div class="name">社　名</div>
+                                <div class="desc">東京自動車興業株式会社</div>
+                            </div>
+                            <div class="box-item">
+                                <div class="name">住　所</div>
+                                <div class="desc">〒135-0051 東京都江東区枝川1-9-4　住友不動産豊洲TKビル3階</div>
+                            </div>
+                            <div class="box-item">
+                                <div class="name">TEL</div>
+                                <div class="desc">03-6850-2122</div>
+                            </div>
+                            <div class="box-item">
+                                <div class="name">FAX</div>
+                                <div class="desc">03-6685-5324</div>
+                            </div>
+                            <div class="box-item">
+                                <div class="name">URL</div>
+                                <div class="desc">http://www.tokyojiko.co.jp</div>
+                            </div>
+                        </div>
+                        <div class="last-line">
+                            <img src="./images/line.svg" alt="">
+                        </div>
+                        <div class="location-box">
+                            <div class="box-item">
+                                <div class="name">社　名</div>
+                                <div class="desc">彌生ヂーゼル工業株式会社</div>
+                            </div>
+                            <div class="box-item">
+                                <div class="name">住　所</div>
+                                <div class="desc">〒134-0086 東京都江戸川区臨海町4-3-1</div>
+                            </div>
+                            <div class="box-item">
+                                <div class="name">TEL</div>
+                                <div class="desc">03-3675-1331</div>
+                            </div>
+                            <div class="box-item">
+                                <div class="name">FAX</div>
+                                <div class="desc">03-3675-1332</div>
+                            </div>
+                            <div class="box-item">
+                                <div class="name">URL</div>
+                                <div class="desc">http://www.yayoidizel.co.jp</div>
+                            </div>
+                        </div>
+                        <div class="last-line">
+                            <img src="./images/line.svg" alt="">
+                        </div>
+                        <div class="title">神奈川エリア</div>
+                        <div class="location-box">
+                            <div class="box-item">
+                                <div class="name">社　名</div>
+                                <div class="desc">株式会社アマギ</div>
+                            </div>
+                            <div class="box-item">
+                                <div class="name">住　所</div>
+                                <div class="desc">〒252-0131 神奈川県相模原市緑区西橋本1-9-2</div>
+                            </div>
+                            <div class="box-item">
+                                <div class="name">TEL</div>
+                                <div class="desc">042-773-6748</div>
+                            </div>
+                            <div class="box-item">
+                                <div class="name">FAX</div>
+                                <div class="desc">042-773-8287</div>
+                            </div>
+                            <div class="box-item">
+                                <div class="name">URL</div>
+                                <div class="desc">http://www.amagi-group.com/</div>
+                            </div>
+                        </div>
+                        <div class="last-line">
+                            <img src="./images/line.svg" alt="">
+                        </div>
+                        <div class="location-box">
+                            <div class="box-item">
+                                <div class="name">社　名</div>
+                                <div class="desc">神奈川石油販売株式会社</div>
+                            </div>
+                            <div class="box-item">
+                                <div class="name">住　所</div>
+                                <div class="desc">〒244-0803 神奈川県横浜市戸塚区平戸町150</div>
+                            </div>
+                            <div class="box-item">
+                                <div class="name">TEL</div>
+                                <div class="desc">045-822-6441</div>
+                            </div>
+                            <div class="box-item">
+                                <div class="name">FAX</div>
+                                <div class="desc">045-821-7925</div>
+                            </div>
+                            <div class="box-item">
+                                <div class="name">URL</div>
+                                <div class="desc">http://www.kanaseki.biz/index.html</div>
+                            </div>
+                        </div>
+                        <div class="last-line">
+                            <img src="./images/line.svg" alt="">
+                        </div>
+                        <div class="location-box">
+                            <div class="box-item">
+                                <div class="name">社　名</div>
+                                <div class="desc">有限会社日生自動車工業</div>
+                            </div>
+                            <div class="box-item">
+                                <div class="name">住　所</div>
+                                <div class="desc">〒224-0054 神奈川県横浜市都筑区佐江戸町864</div>
+                            </div>
+                            <div class="box-item">
+                                <div class="name">TEL</div>
+                                <div class="desc">045-933-9091</div>
+                            </div>
+                            <div class="box-item">
+                                <div class="name">FAX</div>
+                                <div class="desc">045-931-0033</div>
+                            </div>
+                            <div class="box-item">
+                                <div class="name">URL</div>
+                                <div class="desc">http://www.nissei.info/</div>
+                            </div>
+                        </div>
+                        <div class="last-line">
+                            <img src="./images/line.svg" alt="">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="bottom-content">
+                    <div class="top-title">
+                        <i class="icon-kumiai_search_icon"></i>地域でみる
+                    </div>
+                    <div class="top-button">
+                        <a href="#">東京エリア</a>
+                        <a href="#">神奈川エリア</a>
+                        <a href="#">埼玉エリア</a>
+                        <a href="#">千葉エリア</a>
+                        <a href="#">茨城エリア</a>
+                        <a href="#">栃木エリア</a>
+                        <a href="#">群馬エリア</a>
+                        <a href="#">すべて</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="monitoring-service">
+        <div class="partner-img">
+            <img class="main-partner" src="<?php echo get_template_directory_uri(); ?>/images/partner-bg-img.png" alt="">
+            <a href="#" class="inner-partner">
+                <img class="img-inner" src="<?php echo get_template_directory_uri(); ?>/images/partner-button.svg" alt="">
+                <p class="text-inner">トラックモニタリングサービスとは</p>
+                <div class="shape-arrow"></div>
+            </a>
+        </div>
+    </section>
+
+    <section id="member-login">
+        <div class="wrap_member_login">
+            <p class="text">Login for exclusive use of the member</p>
+            <a href="#" class="button">組合員専用ページ</a>
+        </div>
+    </section>
+</main>
+<!--End main-->
+
+<?php get_footer(); ?>
