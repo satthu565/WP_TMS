810b3a775d485d9ff17ae743c10f28327046c0a7
minh la <satthutit@gmail.com>
2018-10-09 13:44:07 +0900

    Copy code to katsudozisseki2

---
+++ page-katudozisseki2.php
@@ -0,0 +1,278 @@
+<?php get_header(); ?>
+
+<main class="clear">
+    <section id="header-text-block">
+        <div class="container">
+            <div class="wrap-header-txt">
+                <div class="title">活動実績</div>
+                <div class="desc">Results of Activities</div>
+            </div>
+        </div>
+    </section>
+
+    <section id="katudozisseki2-content">
+        <div class="top-content">
+            <div class="container">
+                <p>｜<a href="">すべて</a>｜<a href="">平成29年度</a>｜<a href="">平成28年度</a>｜<a href="">平成27年度</a>｜<a href="">平成26年度</a>｜<a href="">平成25年度</a>｜<a href="">平成24年度</a>｜<a href="">平成23年度</a>｜</p>
+                <p>｜<a href="">平成22年度</a>｜<a href="">平成21年度</a>｜<a href="">平成20年度</a>｜<a href="">平成19年度</a>｜<a href="">平成18年度</a>｜<a href="">平成17年度</a>｜<a href="">平成16年度</a>｜<a href="">平成15年度</a>｜</p>
+            </div>
+        </div>
+        <div class="main-content">
+            <div class="table-item">
+                <div class="container">
+                    <div class="row">
+                        <p class="title">平成29年度</p>
+                        <div class="wrap-table">
+                            <table>
+                                <tr>
+                                    <td rowspan="5" class="td-type-1">理事会</td>
+                                    <td class="text-right td-type-1">2月</td>
+                                    <td>第3回　事業進捗報告　事業委員会議案決議</td>
+                                </tr>
+                                <tr>
+                                    <td class="text-right td-type-1">3月</td>
+                                    <td>第3回　事業進捗報告　事業委員会議案決裁</td>
+                                </tr>
+                                <tr>
+                                    <td class="text-right td-type-1">6月</td>
+                                    <td>第4回　事業進捗報告　事業委員会議決決裁</td>
+                                </tr>
+                                <tr>
+                                    <td class="text-right td-type-1">9月</td>
+                                    <td>第5回　事業進捗報告　事業委員会議決決裁</td>
+                                </tr>
+                                <tr>
+                                    <td class="text-right td-type-1">12月</td>
+                                    <td>第1回　新規取引・新規取扱商品検討　セミナー詳細検討</td>
+                                </tr>
+                                <tr>
+                                    <td rowspan="4">事業委員会</td>
+                                    <td class="text-right">3月</td>
+                                    <td>第2回　新規取引・新規取扱商品検討　セミナー詳細検討</td>
+                                </tr>
+                                <tr>
+                                    <td class="text-right">6月</td>
+                                    <td>第3回　新規取引・新規取扱商品検討　セミナー詳細検討</td>
+                                </tr>
+                                <tr>
+                                    <td class="text-right">9月</td>
+                                    <td>第4回　新規取引・新規取扱商品検討　セミナー詳細検討</td>
+                                </tr>
+                                <tr>
+                                    <td class="text-right">12月</td>
+                                    <td>５月	第1回　フロント・営業セミナー</td>
+                                </tr>
+                                <tr>
+                                    <td rowspan="5" class="td-type-1">セミナー部会開講</td>
+                                    <td class="text-right td-type-1">5月</td>
+                                    <td>第1回　技術セミナー『安全自動車様による安全講習』　於：安全自動車会議室</td>
+                                </tr>
+                                <tr>
+                                    <td class="text-right td-type-1">7月</td>
+                                    <td>第1回　員外工場見学　出光興産様 千葉製油所見学</td>
+                                </tr>
+                                <tr>
+                                    <td class="text-right td-type-1">8月</td>
+                                    <td>経営セミナー『労働環境と経営状況から考える利益向上セミナー』（山梨県） </td>
+                                </tr>
+                                <tr>
+                                    <td class="text-right td-type-1">9月</td>
+                                    <td>第1回　会員工場見学　神奈川石油販売</td>
+                                </tr>
+                                <tr>
+                                    <td class="text-right td-type-1">11月</td>
+                                    <td></td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="table-item">
+                <div class="container">
+                    <div class="row">
+                        <p class="title">平成28年度</p>
+                        <div class="wrap-table">
+                            <table>
+                                <tr>
+                                    <td rowspan="5" class="td-type-1">理事会</td>
+                                    <td class="text-right td-type-1">2月</td>
+                                    <td>第1回　総会議案について</td>
+                                </tr>
+                                <tr>
+                                    <td class="text-right td-type-1">3月</td>
+                                    <td>第3回　事業進捗報告　事業委員会議案決議</td>
+                                </tr>
+                                <tr>
+                                    <td class="text-right td-type-1">6月</td>
+                                    <td>第3回　事業進捗報告　事業委員会議案決裁</td>
+                                </tr>
+                                <tr>
+                                    <td class="text-right td-type-1">9月</td>
+                                    <td>第4回　事業進捗報告　事業委員会議決決裁</td>
+                                </tr>
+                                <tr>
+                                    <td class="text-right td-type-1">12月</td>
+                                    <td>第5回　事業進捗報告　事業委員会議決決裁</td>
+                                </tr>
+                                <tr>
+                                    <td rowspan="4">事業委員会</td>
+                                    <td class="text-right">3月</td>
+                                    <td>第1回　新規取引・新規取扱商品検討　セミナー詳細検討</td>
+                                </tr>
+                                <tr>
+                                    <td class="text-right">6月</td>
+                                    <td>第2回　新規取引・新規取扱商品検討　セミナー詳細検討</td>
+                                </tr>
+                                <tr>
+                                    <td class="text-right">9月</td>
+                                    <td>第3回　新規取引・新規取扱商品検討　セミナー詳細検討</td>
+                                </tr>
+                                <tr>
+                                    <td class="text-right">12月</td>
+                                    <td>第4回　新規取引・新規取扱商品検討　セミナー詳細検討</td>
+                                </tr>
+                                <tr>
+                                    <td rowspan="5"class="td-type-1">セミナー部会開講</td>
+                                    <td class="text-right td-type-1">5月</td>
+                                    <td>５月	第1回　フロント・営業セミナー</td>
+                                </tr>
+                                <tr>
+                                    <td class="text-right td-type-1">7月</td>
+                                    <td>第1回　技術セミナー『安全自動車様による安全講習』　於：安全自動車会議室</td>
+                                </tr>
+                                <tr>
+                                    <td class="text-right td-type-1">8月</td>
+                                    <td>第1回　員外工場見学　出光興産様 千葉製油所見学</td>
+                                </tr>
+                                <tr>
+                                    <td class="text-right td-type-1">9月</td>
+                                    <td>経営セミナー『労働環境と経営状況から考える利益向上セミナー』（山梨県） </td>
+                                </tr>
+                                <tr>
+                                    <td class="text-right td-type-1">11月</td>
+                                    <td>第1回　会員工場見学　神奈川石油販売</td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="table-item">
+                <div class="container">
+                    <div class="row">
+                        <p class="title">平成27年度</p>
+                        <div class="wrap-table">
+                            <table>
+                                <tr>
+                                    <td rowspan="5"class="td-type-1">理事会</td>
+                                    <td class="text-right td-type-1">2月</td>
+                                    <td>第1回　総会議案について</td>
+                                </tr>
+                                <tr>
+                                    <td class="text-right td-type-1">3月</td>
+                                    <td>第3回　事業進捗報告　事業委員会議案決議</td>
+                                </tr>
+                                <tr>
+                                    <td class="text-right td-type-1">6月</td>
+                                    <td>第3回　事業進捗報告　事業委員会議案決裁</td>
+                                </tr>
+                                <tr>
+                                    <td class="text-right td-type-1">9月</td>
+                                    <td>第4回　事業進捗報告　事業委員会議決決裁</td>
+                                </tr>
+                                <tr>
+                                    <td class="text-right td-type-1">12月</td>
+                                    <td>第5回　事業進捗報告　事業委員会議決決裁</td>
+                                </tr>
+                                <tr>
+                                    <td rowspan="4">事業委員会</td>
+                                    <td class="text-right">3月</td>
+                                    <td>第1回　新規取引・新規取扱商品検討　セミナー詳細検討</td>
+                                </tr>
+                                <tr>
+                                    <td class="text-right">6月</td>
+                                    <td>第2回　新規取引・新規取扱商品検討　セミナー詳細検討</td>
+                                </tr>
+                                <tr>
+                                    <td class="text-right">9月</td>
+                                    <td>第3回　新規取引・新規取扱商品検討　セミナー詳細検討</td>
+                                </tr>
+                                <tr>
+                                    <td class="text-right">12月</td>
+                                    <td>第4回　新規取引・新規取扱商品検討　セミナー詳細検討</td>
+                                </tr>
+                                <tr>
+                                    <td rowspan="5" class="td-type-1">セミナー部会開講</td>
+                                    <td class="text-right td-type-1">5月</td>
+                                    <td>５月	第1回　フロント・営業セミナー</td>
+                                </tr>
+                                <tr>
+                                    <td class="text-right td-type-1">7月</td>
+                                    <td>第1回　技術セミナー『安全自動車様による安全講習』　於：安全自動車会議室</td>
+                                </tr>
+                                <tr>
+                                    <td class="text-right td-type-1">8月</td>
+                                    <td>第1回　員外工場見学　出光興産様 千葉製油所見学</td>
+                                </tr>
+                                <tr>
+                                    <td class="text-right td-type-1">9月</td>
+                                    <td>経営セミナー『労働環境と経営状況から考える利益向上セミナー』（山梨県） </td>
+                                </tr>
+                                <tr>
+                                    <td class="text-right td-type-1">11月</td>
+                                    <td>第1回　会員工場見学　神奈川石油販売</td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="wrap-pagination">
+            <div class="container">
+                <div class="pagination">
+                    <div class="page-number">1 / 5</div>
+                    <div class="page-list">
+                        <ul>
+                            <li><a href=""><i class="icon-arrow-left"></i></a></li>
+                            <li><a href="">1</a></li>
+                            <li><a href="">2</a></li>
+                            <li><a href="">3</a></li>
+                            <li><a href="">4</a></li>
+                            <li><a href="">5</a></li>
+                            <li><a href=""><i class="icon-arrow-right"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="bottom-content clear">
+            <div class="container">
+                <p>｜<a href="">すべて</a>｜<a href="">平成29年度</a>｜<a href="">平成28年度</a>｜<a href="">平成27年度</a>｜<a href="">平成26年度</a>｜<a href="">平成25年度</a>｜<a href="">平成24年度</a>｜<a href="">平成23年度</a>｜</p>
+                <p>｜<a href="">平成22年度</a>｜<a href="">平成21年度</a>｜<a href="">平成20年度</a>｜<a href="">平成19年度</a>｜<a href="">平成18年度</a>｜<a href="">平成17年度</a>｜<a href="">平成16年度</a>｜<a href="">平成15年度</a>｜</p>
+            </div>
+        </div>
+    </section>
+
+    <section id="monitoring-service">
+        <div class="partner-img">
+            <img class="main-partner" src="./images/partner-bg-img.png" alt="">
+            <a href="#" class="inner-partner">
+                <img class="img-inner" src="./images/partner-button.svg" alt="">
+                <p class="text-inner">トラックモニタリングサービスとは</p>
+                <div class="shape-arrow"></div>
+            </a>
+        </div>
+    </section>
+
+    <section id="member-login">
+        <div class="wrap_member_login">
+            <p class="text">Login for exclusive use of the member</p>
+            <a href="#" class="button">組合員専用ページ</a>
+        </div>
+    </section>
+</main>
+<!--End main-->
+
+<?php get_footer(); ?>
