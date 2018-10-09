17f695a7e2b9b0581dcf5767d64bda639a4bf93f
minh la <satthutit@gmail.com>
2018-10-09 13:25:34 +0900

    Add and fix link page-katudoyotei

---
+++ page-katudoyotei.php
@@ -0,0 +1,170 @@
+<?php get_header(); ?>
+
+<main class="clear">
+    <section id="header-text-block">
+        <div class="container">
+            <div class="wrap-header-txt">
+                <div class="title">組合について</div>
+                <div class="desc">Kanto Truck Monitoring Cooperative Association</div>
+            </div>
+        </div>
+    </section>
+
+    <section id="katsudo-content">
+        <div class="container">
+            <div class="row">
+                <div class="katsudo-title">
+                    <p>
+                        平成29年度活動予定
+                    </p>
+                </div>
+                <div class="katsudo-table">
+                    <table>
+                        <tr>
+                            <th colspan="2">開催日</th>
+                            <th>開催行事</th>
+                            <th>開催場所</th>
+                            <th>資料DL</th>
+                        </tr>
+                        <tr>
+                            <td rowspan="3" class="td-type-1 width-11">2月</th>
+                            <td class="td-type-1 width-11">2日(木)</td>
+                            <td class="td-type-3">第1回　理事会</td>
+                            <td>東京自動車興業㈱本社</td>
+                            <td class="text-center"><img src="<?php echo get_template_directory_uri(); ?>/images/download.svg" alt=""></td>
+                        </tr>
+                        <tr>
+                            <td class="td-type-1 width-11">27日(月)</td>
+                            <td class="td-type-3">第14回　通常総会</td>
+                            <td rowspan="2">あいおいニッセイ同和損害保険㈱ <br>
+                                新宿第一支社
+                            </td>
+                            <td class="text-center">ー</td>
+                        </tr>
+                        <tr>
+                            <td class="td-type-1 width-11">27日(月)</td>
+                            <td class="td-type-3">第2回　理事会</td>
+                            <td class="text-center"><img src="<?php echo get_template_directory_uri(); ?>/images/download.svg" alt=""></td>
+                        </tr>
+                        <tr>
+                            <td rowspan="2" class="td-type-2 width-11">3月</td>
+                            <td rowspan="2" class="td-type-2 width-11">15日(水)</td>
+                            <td class="td-type-4">第1回　事業委員会</td>
+                            <td rowspan="2">東京</td>
+                            <td class="text-center">ー</td>
+                        </tr>
+                        <tr>
+                            <td class="td-type-4">第３回　理事会</td>
+                            <td class="text-center"><img src="<?php echo get_template_directory_uri(); ?>/images/download.svg" alt=""></td>
+                        </tr>
+                        <tr>
+                            <td class="td-type-1 width-11">4月</td>
+                            <td class="td-type-1 width-11">12日(水)</td>
+                            <td class="td-type-3">第１回　工場見学会(会員)</td>
+                            <td>交運社</td>
+                            <td class="text-center"><img src="<?php echo get_template_directory_uri(); ?>/images/download.svg" alt=""></td>
+                        </tr>
+                        <tr>
+                            <td class="td-type-2 width-11">5月</td>
+                            <td class="td-type-2 width-11">25日(木)</td>
+                            <td class="td-type-4">第１回　フロントセミナー</td>
+                            <td>未定</td>
+                            <td class="text-center">ー</td>
+                        </tr>
+                        <tr>
+                            <td rowspan="2" class="td-type-1 width-11">6月</td>
+                            <td rowspan="2" class="td-type-1 width-11">14日(水)</td>
+                            <td class="td-type-3">第２回　事業委員会</td>
+                            <td rowspan="2" >東京</td>
+                            <td class="text-center"><img src="<?php echo get_template_directory_uri(); ?>/images/download.svg" alt=""></td>
+
+                        </tr>
+                        <tr>
+                            <td class="td-type-3">第４回　理事会</td>
+                            <td class="text-center"><img src="<?php echo get_template_directory_uri(); ?>/images/download.svg" alt=""></td>
+                        </tr>
+                        <tr>
+                            <td class="td-type-2 width-11">7月</td>
+                            <td class="td-type-2 width-11">19日(水)</td>
+                            <td class="td-type-4">第１回　技術セミナー</td>
+                            <td>未定</td>
+                            <td class="text-center">ー</td>
+                        </tr>
+                        <tr>
+                            <td class="td-type-1 width-11">8月</td>
+                            <td class="td-type-1 width-11">23日(水)</td>
+                            <td class="td-type-3">第２回　工場見学会(員外)</td>
+                            <td>未定</td>
+                            <td class="text-center">ー</td>
+                        </tr>
+                        <tr>
+                            <td rowspan="3" class="td-type-2 width-11">9月</td>
+                            <td rowspan="3" class="td-type-2 width-11">
+                                <p>10日(日)</p>
+                                <br><br>
+                                <p class="rotate">〜</p>
+                                <br><br>
+                                <p>11日(月)</p>
+                            </td>
+                            <td class="td-type-4">経営者セミナー</td>
+                            <td rowspan="3">未定</td>
+                            <td class="text-center">ー</td>
+                        </tr>
+                        <tr>
+                            <td class="td-type-4">第３回　事業委員会</td>
+                            <td class="text-center">ー</td>
+                        </tr>
+                        <tr>
+                            <td class="td-type-4">第５回　理事会</td>
+                            <td class="text-center">ー</td>
+                        </tr>
+                        <tr>
+                            <td class="td-type-1 width-11">11月</td>
+                            <td class="td-type-1 width-11">15日(水)</td>
+                            <td class="td-type-3">第２回　技術セミナー</td>
+                            <td>未定</td>
+                            <td class="text-center">ー</td>
+                        </tr>
+                        <tr>
+                            <td rowspan="2" class="td-type-2 width-11">12月</td>
+                            <td rowspan="2" class="td-type-2 width-11">13日(水)</td>
+                            <td class="td-type-4">第４回　事業委員会
+                            </td>
+                            <td rowspan="2">東京	</td>
+                            <td class="text-center"><img src="<?php echo get_template_directory_uri(); ?>/images/download.svg" alt=""></td>
+                        </tr>
+                        <tr>
+                            <td class="td-type-4">第６回　理事会</td>
+                            <td class="text-center"><img src="<?php echo get_template_directory_uri(); ?>/images/download.svg" alt=""></td>
+                        </tr>
+                    </table>
+                </div>
+                <div class="footer-text">
+                    <p>※資料ダウンロードには、組合員ID・PASSWORDが必要です</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="monitoring-service">
+        <div class="partner-img">
+            <img class="main-partner" src="<?php echo get_template_directory_uri(); ?>/images/partner-bg-img.png" alt="">
+            <a href="#" class="inner-partner">
+                <img class="img-inner" src="<?php echo get_template_directory_uri(); ?>/images/partner-button.svg" alt="">
+                <p class="text-inner">トラックモニタリングサービスとは</p>
+                <div class="shape-arrow"></div>
+            </a>
+        </div>
+    </section>
+
+    <section id="member-login">
+        <div class="wrap_member_login">
+            <p class="text">Login for exclusive use of the member</p>
+            <a href="#" class="button">組合員専用ページ</a>
+        </div>
+    </section>
+</main>
+<!--End main-->
+
+
+<?php get_footer(); ?>
