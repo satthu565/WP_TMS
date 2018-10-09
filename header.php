548022fbf88709690932f0b5a7eec8ba0d574024
minh la <satthutit@gmail.com>
2018-10-09 10:16:36 +0900

    Convert Header

--- header.php
+++ header.php
@@ -13,46 +13,67 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
 	<?php wp_head(); ?>
 </head>
 
-<body <?php body_class(); ?>>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'tms' ); ?></a>
+<body id="st-container" <?php body_class('st-container'); ?>>
+<div class="st-pusher">
+    <header>
+        <div class="top-header container">
+            <div class="logo">
+                <h1>
+                    <a href="<?php echo esc_url( home_url( '/' ) ); ?>">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/logo.svg" alt="">
+                    </a>
+                </h1>
+            </div>
+            <div class="header-button">
+                <a href="#">
+                    <i class="icon-email"></i>
+                    <span>お問い合わせ</span>
+                </a>
+            </div>
+        </div>
+        <div class="menu">
+            <nav class="main-menu">
+                <ul>
+                    <li><a href="#"><i class="icon-menu-1 menu-icon"></i><span class="menu-text">組合について</span></a></li>
+                    <li><a href="#"><i class="icon-menu-2 menu-icon"></i><span class="menu-text">活 動 予 定</span></a></li>
+                    <li><a href="#"><i class="icon-menu-3 menu-icon"></i><span class="menu-text">活 動 実 績</span></a></li>
+                    <li><a href="#"><i class="icon-menu-4 menu-icon"></i><span class="menu-text">会 員 一 覧</span></a></li>
+                    <li><a href="#"><i class="icon-menu-5 menu-icon"></i><span class="menu-text">組 合 加 入</span></a></li>
+                </ul>
+                <!--<a href="#" id="pull"></a>-->
+            </nav>
+        </div>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$tms_description = get_bloginfo( 'description', 'display' );
-			if ( $tms_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $tms_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+        <nav class="st-menu st-effect-3" id="menu-3">
+            <div class="sp-menu-title">
+                <h3>メニュー <button data-effect="st-effect-3" id="close_btn">閉じる</button></h3><!--<img src="./images/nav-icon.png" alt="">-->
+            </div>
+            <ul>
+                <li><a href="#"><i class="icon-menu-1 menu-icon"></i><span class="menu-text">組合について</span></a></li>
+                <li><a href="#"><i class="icon-menu-2 menu-icon"></i><span class="menu-text">活 動 予 定</span></a></li>
+                <li><a href="#"><i class="icon-menu-3 menu-icon"></i><span class="menu-text">活 動 実 績</span></a></li>
+                <li><a href="#"><i class="icon-menu-4 menu-icon"></i><span class="menu-text">会 員 一 覧</span></a></li>
+                <li><a href="#"><i class="icon-menu-5 menu-icon"></i><span class="menu-text">組 合 加 入</span></a></li>
+            </ul>
+        </nav>
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'tms' ); ?></button>
-			<?php
-			wp_nav_menu( array(
-				'theme_location' => 'menu-1',
-				'menu_id'        => 'primary-menu',
-			) );
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+        <div class="sp-menu">
+            <div class="st-content"><!-- this is the wrapper for the content -->
+                <div class="st-content-inner"><!-- extra div for emulating position:fixed of the menu -->
+                    <div class="main clearfix">
+                        <div id="st-trigger-effects" class="column">
+                            <button data-effect="st-effect-3"><img src="<?php echo get_template_directory_uri(); ?>/images/nav-icon.png" alt=""></button>
+                        </div>
+                    </div><!-- /main -->
+                </div><!-- /st-content-inner -->
+            </div><!-- /st-content -->
+        </div>
+    </header>
+    <!--End header-->
 
-	<div id="content" class="site-content">
